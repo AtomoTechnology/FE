ca21a2353c2d7c79a29f3ac6446280df6966da5f
AtomoTechnology <Atomotechnology@gmail.com>
2022-03-14 22:04:59 -0300

    mydata form modal

--- AppYuyu/src/app/components/mydata/mydata.module.ts
+++ AppYuyu/src/app/components/mydata/mydata.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { MydataRoutingModule } from './mydata-routing.module';
 import { MydataComponent } from './mydata/mydata.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    MydataComponent
-  ],
-  imports: [
-    CommonModule,
-    MydataRoutingModule
-  ]
+  declarations: [MydataComponent],
+  imports: [CommonModule, MydataRoutingModule, ReactiveFormsModule],
 })
-export class MydataModule { }
+export class MydataModule {}
--- AppYuyu/src/app/components/mydata/mydata/mydata.component.ts
+++ AppYuyu/src/app/components/mydata/mydata/mydata.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { ApiController } from 'src/app/controller/ApiController';
 import { GenericService } from 'src/app/services/generic.service';
@@ -11,7 +12,35 @@ import { GenericService } from 'src/app/services/generic.service';
 export class MydataComponent implements OnInit {
   private ctrl = new ApiController();
   public user: any = null;
-  constructor(private gen: GenericService, private actRoute: ActivatedRoute) {
+
+  private reg: RegExp =
+    /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  userForm: FormGroup = this.fb.group({
+    id: 0,
+    firstname: ['', [Validators.required]],
+    lastname: ['', [Validators.required]],
+    roleId: [''],
+    email: ['', [Validators.required, , Validators.pattern(this.reg)]],
+
+    adress: ['', [Validators.required]],
+    phone: ['', [Validators.required]],
+  });
+
+  passwordForm: FormGroup = this.fb.group({
+    oldPassword: [
+      '',
+      [Validators.required, Validators.minLength(5), Validators.maxLength(25)],
+    ],
+    password: [
+      '',
+      [Validators.required, Validators.minLength(5), Validators.maxLength(25)],
+    ],
+  });
+  constructor(
+    private gen: GenericService,
+    private fb: FormBuilder,
+    private actRoute: ActivatedRoute
+  ) {
     var idStr = this.actRoute.snapshot.paramMap.get('id')!;
     var id = parseInt(idStr);
     this.getById(id);
@@ -22,5 +51,13 @@ export class MydataComponent implements OnInit {
     this.gen.GetById(id, this.ctrl.user).subscribe((data: any) => {
       this.user = data.data;
     });
+  }
+
+  UpdatePassword() {
+    console.log(this.passwordForm.value);
+  }
+
+  toggleModal() {
+    document.querySelector('.modal-update-user')?.classList.toggle('hidden');
   }
 }
